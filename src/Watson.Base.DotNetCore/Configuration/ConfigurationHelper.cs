56db8aa2cba289b00d1484cf2739c5b35800ed7e
watson <watson1029@163.com>
2018-03-07 12:04:35 +0800

    Configuration has publish.

---
+++ src/Watson.Base.DotNetCore/Configuration/ConfigurationHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watson.Base.DotNetCore
+{
+    public class ConfigurationHelper
+    {
+        public static IConfiguration Configuration { get; set; }
+        static ConfigurationHelper()
+        {
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = "appsettings.json", ReloadOnChange = true })
+            .Build();
+        }
+    }
+}
